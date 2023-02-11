9e8313bb93283e4b6bb4d295b8406e24ce008315
Mike Gudenau <mikegudenau@gmail.com>
2023-02-11 14:20:40 -0700

    add solutions to number 1 - 3

--- functions.js
+++ functions.js
@@ -4,29 +4,54 @@
 // Use console.log() to see the return value in the console.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
+function greeting() {
+    return 'Howdy parter!';
+}
+
+var newGeneral = greeting();
+var newGeneral2 = greeting();
+
+console.log(newGeneral);
+console.log(newGeneral2);
 
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
+function customGreeting(name) {
+    return `You've been awarded the eagle medallion, ${name}. Congratulations!`;
+}
 
+var newString = customGreeting('Mike');
+var newString2 = customGreeting('Andre');
+
+console.log(newString);
+console.log(newString2);
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+function greetPerson(first, middle, last) {
+    return `Your full name, ${first} ${middle} ${last} is beautiful!`;
+}
+
+var nameString = greetPerson('Anthony', 'Partick', 'Owens');
+var nameString2 = greetPerson('Megan', 'Jarita', 'Quinn');
+
+console.log(nameString);
+console.log(nameString2);
 
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
 
-
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
 
-checkStock(4, "Coffee");
-// => "Coffee is stocked"
+// checkStock(4, "Coffee");
+// // => "Coffee is stocked"
 
-checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
+// checkStock(3, "Tortillas");
+// // => "Tortillas - running LOW"
 
-checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
+// checkStock(0, "Cheese");
+// // => "Cheese - OUT of stock!"
 
-checkStock(1, "Salsa");
-// => "Salsa - running LOW"
+// checkStock(1, "Salsa");
+// // => "Salsa - running LOW"
