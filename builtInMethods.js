2c37ccec3874aaf8b04949dbf9311d9c673d3abc
Mike Gudenau <mikegudenau@gmail.com>
2023-02-11 13:46:21 -0700

    add the solutions to section 1-3

--- builtInMethods.js
+++ builtInMethods.js
@@ -8,13 +8,19 @@
 // The return value is "hello world"
 "Hello World".toLowerCase();
 
-
+// The .includes() method is called on the string "Hello World"
+// The argument "Hello" is passed; .includes() has the job of checking if certain values are included in the string. This returns a boolean value. 
+// the return value is true
 "Hello World".includes("Hello");
 
-
+// The .endsWith() method is called on the string "Hello World"
+// The argument "Hello" is passed. .endsWith() has the job of checking if the string ends with specified string. This returns a boolean value. 
+// the return value is false
 "Hello World".endsWith("Hello");
 
-
+// The .endsWith() method is called on the string "Hello World"
+// The argument "Hello" is passed. .endsWith() has the job of checking if the string ends with specified string. This returns a boolean value. 
+// the return value is true
 "Hello World".endsWith("rld");
 
 
@@ -33,6 +39,20 @@
 var firstName = "Jeff";
 console.log(firstName.startsWith("J"));
 
+// The toUpperCase() method is called on the currentMood variable, which stores the string "excited".
+// The toUpperCase() method does not pass an argument. This method will return the called upon string and convert the string to uppercase.
+// In this example, the return value will return the string "EXCITED".
+// The console.log() statement prints the return value of the toUpperCase() method ("EXCITED") to the console.
+var currentMood = "excited";
+console.log(currentMood.toUpperCase());
+
+// The repeat() method is called on the currentMood variable, which stores the string "energized ".
+// The repeat() method passes the argument (5). This method will return and construct a new string of repeated specified number of copies and concatenate them together. 
+// In this example, the return value will return "energized" 5 times. 
+// The console.log() statement prints the string "I currently feel" and adds "energized " concatenated together. 
+var currentMood = "energized ";
+console.log(`I currently feel ${currentMood.repeat(5)}.`);
+
 
 
 // SECTION 3: Calling methods on variables assigned to arrays.
@@ -40,3 +60,20 @@ console.log(firstName.startsWith("J"));
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 // Include comments above each method call explaining the impact and return value of that method.
+
+// The push() method is called on the score variable, which stores the array [89, 77, 98, 55, 85, 92].
+// The push() method passes the argument (67, 78); This will add the new elements to the end of the array and return the new length of the array. 
+// In this example, the console.log() statement will print the new array length, which is 8.
+// I used the second console.log() statement check if the two new elements were pushed to the score array. This returned, [89, 77, 98, 55, 85, 92, 67, 78]
+var score = [89, 77, 98, 55, 85, 92];
+console.log(score.push(67, 78));
+console.log(score);
+
+// The concat() method is called on the firstNames variable, which stores the array ['Murphy', 'Tim', 'Sandra'].
+// The concat() method passes the lastNames argument, which is an array which stores the array ['Quinn', 'Adam', 'Cahill'].
+// The concat() method will merge the two arrays.
+// In this example, the console.log() statement will return a new array, [ 'Murphy', 'Tim', 'Sandra', 'Quinn', 'Adam', 'Cahill' ]
+// This will not alter the orignial arrays, only return a new array.
+var firstNames = ['Murphy', 'Tim', 'Sandra'];
+var lastNames = ['Quinn', 'Adam', 'Cahill'];
+console.log(firstNames.concat(lastNames));
